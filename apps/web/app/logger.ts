e838ab1fb3945df2536ac3ca771d6c7b6ebdcf30
Eric Leung <gteceric@gmail.com>
2026-04-29 14:22:31 +0800

    keep logger silent during testing

--- apps/web/app/logger.ts
+++ apps/web/app/logger.ts
@@ -1,1 +1,7 @@
-export const logger: Pick<Console, "info" | "warn"> = console;
+const isNodeTestRun = process.env.NODE_ENV === "test";
+const noop = () => {};
+
+// keep logger silent during testing
+export const logger: Pick<Console, "info" | "warn"> = isNodeTestRun
+  ? { info: noop, warn: noop }
+  : console;
